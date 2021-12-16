f2b0fe70bfd4fc04c560357b775e84920dcb7268
call5h5ong <call5h5ong@gmail.com>
2021-12-16 19:47:56 +0900

    feat: github repo의 변경 사항 정보를 받아 노션 페이지 생성
    `github/webhooks`로 요청이 들어오면 그 안의 커밋들의 정보를 바탕으로 노션 페이지를 생성함.
    
    `github/webhooks`로 들어오는 요청은 github webhooks에서 옴. 특정 이벤트가 발생했을 때 github에서 지정된 url로 이벤트의 정보가 포함된 요청을 보냄.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
 import "dotenv/config";
-import {
-  notionCreatePageWithCommit,
-  notionGetPagesTitles,
-} from "./libs/notion";
-import { githubGetCommits } from "./libs/github";
-import { compareTitleAndCommit } from "./libs/common";
+import express from "express";
+import { notionCreatePageWithCommit } from "./libs/notion";
+import { splitCommitMessage } from "./libs/common";
 
-(async () => {
-  const githubCommitData = await githubGetCommits();
-  const notionPagesTitle = await notionGetPagesTitles();
+const app = express();
 
-  if (!compareTitleAndCommit(githubCommitData[0], notionPagesTitle)) {
-    await notionCreatePageWithCommit(githubCommitData[0]);
+app.use(express.json());
+
+/**
+ * Github Push Webhook 요청을 받음
+ * 들어온 webhook 요청에서 commit들을 가져와 새로운 페이지를 생성함.
+ */
+app.post("/github/webhooks", async (req, res) => {
+  const commits = req.body.commits;
+
+  if (commits) {
+    console.log(commits);
+    for (const { id, message, timestamp } of commits) {
+      await notionCreatePageWithCommit({
+        sha: id,
+        message: splitCommitMessage(message),
+        createdAt: timestamp,
+      });
+    }
+
+    res.sendStatus(200).end();
   } else {
-    console.log(`It's already exists!`);
+    res.sendStatus(400).end();
   }
-})();
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`[info] server listening on port ${process.env.PORT} `);
+});
--- src/libs/common.ts
+++ src/libs/common.ts
@@ -9,3 +9,12 @@ export const compareTitleAndCommit = (
   const result = titlesData.some((title) => title.sha === commit.sha);
   return result;
 };
+
+/**
+ * 여러 줄로 된 커밋을 \n을 기준으로 자른 다음 첫번째 줄을 반환함
+ * @param commitMessage 커밋 메세지
+ * @returns 커밋의 첫번째 줄
+ */
+export const splitCommitMessage = (commitMessage: string): string => {
+  return commitMessage.split("\n")[0];
+};
